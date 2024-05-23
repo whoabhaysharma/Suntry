608ce82099d8a54d3613387f752c70b19be38447
abhaysharma2719 <abhay@techcrumb.com>
2024-05-23 11:01:15 +0530

    initial commit

---
+++ Suntry.js
@@ -0,0 +1,35 @@
+class Suntry {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync('Suntry', (compilation, callback) => {
+      Object.keys(compilation.assets).forEach((assetName) => {
+        if (assetName.endsWith('.js')) {
+          let source = compilation.assets[assetName].source();
+          const { catchFunction } = this.options;
+
+          source = `
+            (function() {
+              const customCatchFunction = ${catchFunction.toString()};
+              try {
+                ${source}
+              } catch (error) {
+                customCatchFunction(error);
+              }
+            })();
+          `;
+
+          compilation.assets[assetName] = {
+            source: () => source,
+            size: () => source.length
+          };
+        }
+      });
+      callback();
+    });
+  }
+}
+
+module.exports = Suntry;
